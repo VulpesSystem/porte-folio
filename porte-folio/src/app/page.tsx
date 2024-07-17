86035615a19d0cf598850b3280e16edb6fdbd8e9
VulpesSystem <122926415+VulpesSystem@users.noreply.github.com>
2024-07-16 20:43:58 -0400

    good to start for real now

--- porte-folio/src/app/page.tsx
+++ porte-folio/src/app/page.tsx
@@ -3,9 +3,13 @@ export default function Home() {
     <div className="relative h-screen">
       <div className="bg-cream rounded-lg w-8/12 h-1/2 absolute inset-0 m-auto p-5">
         <div className="bg-pastel-gradient rounded p-1 h-full">
-          <div className="bg-cream rounded h-full p-3">
-            <p className="text-blackish font-medium text-center text-9xl font-exo">BOOP ! </p>
-            <p className="text-blackish font-medium">just trying to see how things go, nothing to see here</p>
+          <div className="bg-blackish p-0.5 h-full rounded">
+            <div className="bg-pastel-gradient rounded p-0.5 h-full">
+              <div className="bg-cream rounded h-full p-3">
+                <p className="text-blackish font-medium text-center text-9xl font-exo">BOOP ! </p>
+                <p className="text-blackish font-medium">just trying to see how things go, nothing to see here</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
