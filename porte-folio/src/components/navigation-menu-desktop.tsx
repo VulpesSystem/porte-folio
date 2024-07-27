4fea1824d045cf350597c492c7c439675bb61db7
VulpesSystem <122926415+VulpesSystem@users.noreply.github.com>
2024-07-27 12:01:36 -0400

    only prepped files. Not much done but more than any of you so im awesome - Klaus

---
+++ porte-folio/src/components/navigation-menu-desktop.tsx
@@ -0,0 +1,9 @@
+export default function NavigationMenuDesktop() {
+  return (
+    <div className="">
+      <div className="absolute top-16 right-0">
+        <p className="text-cream">nav nav nav this is hard to start working</p>
+      </div>
+    </div>
+  );
+}
